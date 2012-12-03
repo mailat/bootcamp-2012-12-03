ed890c52cbee2e570af2e802deb840e9d478be59
Marius Mailat <marius.mailat@appseleration.com>
2012-12-04 00:00:06 +0100

    + nice view design

--- Yamba/src/com/marakana/yamba/StatusActivity.java
+++ Yamba/src/com/marakana/yamba/StatusActivity.java
@@ -2,21 +2,26 @@ package com.marakana.yamba;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+//import android.view.Menu;
 
 public class StatusActivity extends Activity {
 
+//	EditText editText;
+//	Button updateButton;
+//	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.status);
+		
+		//get the text entered by user
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.status, menu);
-		return true;
-	}
+//
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.status, menu);
+//		return true;
+//	}
 
 }
