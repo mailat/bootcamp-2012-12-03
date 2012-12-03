aee4c396f67e04fd511e3019abfd5369cb77ec54
Marius Mailat <marius.mailat@appseleration.com>
2012-12-04 00:24:36 +0100

    + update gui with hint

--- Yamba/src/com/marakana/yamba/StatusActivity.java
+++ Yamba/src/com/marakana/yamba/StatusActivity.java
@@ -1,27 +1,37 @@
 package com.marakana.yamba;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.app.Activity;
-//import android.view.Menu;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener{
+	EditText editText;
+	Button updateButton;
 
-//	EditText editText;
-//	Button updateButton;
-//	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.status);
 		
 		//get the text entered by user
+		editText = (EditText) findViewById(R.id.editText);
+		
+		//updateButton
+		updateButton = (Button) findViewById(R.id.buttonUpdate);
+		updateButton.setOnClickListener(this);
 	}
-//
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.status, menu);
-//		return true;
-//	}
+	
+	public void onClick (View v)
+	{
+		//send update to yamba.marakana.com
+		//TODO
+		
+		//log the message
+		Log.d ("Yamba", "Message sent:" + editText.getText().toString());
+	}
 
 }
