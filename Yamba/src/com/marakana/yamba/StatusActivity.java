bf1f0618127e39fa803b9b164c5fd14d96e49c76
Marius Mailat <marius.mailat@appseleration.com>
2012-12-04 01:22:27 +0100

    + exception handling post

--- Yamba/src/com/marakana/yamba/StatusActivity.java
+++ Yamba/src/com/marakana/yamba/StatusActivity.java
@@ -2,9 +2,11 @@ package com.marakana.yamba;
 
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.app.Activity;
 import android.util.Log;
 import android.view.View;
@@ -36,15 +38,36 @@ public class StatusActivity extends Activity implements OnClickListener{
 	public void onClick (View v)
 	{
 		//send update to yamba.marakana.com
-		try
-		{
-			twitter.setStatus(editText.getText().toString());
-			Log.d ("Yamba", "Message sent:" + editText.getText().toString());
-		}
-		catch (TwitterException e)
-		{
-			Log.e("Yamba", e.toString());
-		}
+		String status = editText.getText().toString();
+		new PostToTwitter().execute(status);
 	}
 
+	class PostToTwitter extends AsyncTask<String, Integer, String>{
+
+		@Override
+		protected String doInBackground(String... statuses) {	
+			try
+			{
+				Twitter.Status status = twitter.setStatus(editText.getText().toString());
+				Log.d ("Yamba", "Message sent:" + editText.getText().toString());
+				return (status.text);
+			}
+			catch (TwitterException e)
+			{
+				Log.e("Yamba", e.toString());
+				return "Failed to post";
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+		}
+		
+	}
 }
